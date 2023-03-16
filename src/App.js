3c0fcefa87079084fe6307e2cf874de50d046e49
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-03-16 15:48:00 -0300

    Atualiza o componente ColecaoDestaque

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import Banner from "./components/Banner";
+// import Footer from "./components/Banner";
+
+import { Container} from '@mui/material';
+
+import Banner from "./components/Banner";
+import ColecaoDestaque from './components/ColecaoDestaque';
+import Home from "./pages/Home";
+
+
+
+export default function App() 
+{
+  return(
+    <>
+      {/* <Home/> */}
+      {/* <Banner /> */}
+      <ColecaoDestaque />
+    </>
+  )
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+// import Banner from "./components/Banner"  -- assim nao deu certo;
+// import Banner from '../../components/Banner';
+import ColecaoDestaque from '../../components/ColecaoDestaque';
+
+export default function Home() 
+{
+    return(
+        <>
+      
+        {/* <Banner/> */}
+
+        <ColecaoDestaque />
+
+        </>
+    )
+}
